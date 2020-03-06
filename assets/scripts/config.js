2bc4d529fe187fdf24acdb68744e48dde922ce1e
lindsay-kaufman <linds234@gmail.com>
2020-03-06 16:19:05 -0500

    Updated production URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://tastebuds12345.herokuapp.com/',
+  production: 'https://tastebuds12345.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
